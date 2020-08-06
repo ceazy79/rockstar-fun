b4244694d93f303e7d1252a65e1edbfd065b8b96
ceazy79 <3862817+ceazy79@users.noreply.github.com>
2020-08-06 08:54:11 +0200

    Initial commit

---
+++ Power Metal Even Better.rs
@@ -0,0 +1,15 @@
+The fight is 'til the last breath
+Let my wrath be "Death " 
+My enemy is near
+Knock my enemy down, down
+The end is fear
+Let my wrath be with my enemy
+
+Pure Steel is mysterious
+Glamouring Power is furious
+My fate is fatal
+The fighter says  Metal 
+Let the path be him
+Let my wrath be with " all but", him
+
+SCREAM my wrath
---
+++ Power Metal.rs
@@ -0,0 +1,12 @@
+https://codewithrockstar.com/online
+
+Let my wrath be "Death " 
+My enemy is near
+Knock my enemy down, down
+
+Let power be my wrath with my enemy
+The hammer is " Metal "
+Let power be with " all but", him
+
+SCREAM power
+
